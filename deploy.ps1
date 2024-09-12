48d2310b454dbbcda81c5a56863ff52918e4f54c
moinkazi123 <155711429+moinkazi123@users.noreply.github.com>
2024-09-12 16:22:20 +0530

    Add files via upload

---
+++ deploy.ps1
@@ -0,0 +1,11 @@
+# Define the source and destination paths
+$sourcePath = "C:\Users\Administrator\Desktop\dotnet-custom-action\source\HelloWorldDotNetFramework\bin\Release\HelloWorldDotNetFramework.exe"
+$destinationPath = "C:\app\HelloWorldDotNetFramework\"
+
+# Copy the executable to the desired folder
+Write-Output "Copying executable to destination folder..."
+Copy-Item -Path $sourcePath -Destination $destinationPath -Force
+
+# Start the console application
+Write-Output "Starting the application..."
+Start-Process -FilePath "$destinationPath\HelloWorldDotNetFramework.exe"
